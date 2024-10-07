0aa33ba87e184f43744d3cea11799b2ed908b736
Andrew Ekenes <84550532+andrewekenes@users.noreply.github.com>
2024-10-07 15:26:42 -0700

    Add files via upload

---
+++ snap.js
@@ -0,0 +1,14 @@
+wait(10);
+reapeatForever{
+    turnD1onToggle(true, High, (allCircuits));
+    turnD1onToggle(true, Low, (allCircuits));
+    turnD1onToggle(true, High, (allCircuits));
+    turnD1onToggle(true, Low, (allCircuits));
+    wait(10);
+    for (var count = 0; count < 0; count++) {
+        turnD1onToggle(true, High, (allCircuits));
+        turnD1onToggle(true, Low, (allCircuits));
+        turnD1onToggle(true, High, (allCircuits));
+        turnD1onToggle(true, Low, (allCircuits));
+    }
+}
